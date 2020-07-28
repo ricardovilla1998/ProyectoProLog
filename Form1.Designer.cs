47fb10e2d663d583b02ca1e0f7883654a27fecf9
irmin98 <iramtrujilloor@ittepic.edu.mx>
2020-07-27 19:27:21 -0600

    consultas con Prolog y C#

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -49,6 +49,8 @@
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -58,31 +60,35 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(535, 531);
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Location = new System.Drawing.Point(434, 422);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(94, 37);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Siguiente";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label_p1_sistemas
             // 
             this.label_p1_sistemas.AutoSize = true;
-            this.label_p1_sistemas.Location = new System.Drawing.Point(126, 52);
+            this.label_p1_sistemas.Location = new System.Drawing.Point(94, 9);
+            this.label_p1_sistemas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_p1_sistemas.Name = "label_p1_sistemas";
-            this.label_p1_sistemas.Size = new System.Drawing.Size(334, 17);
+            this.label_p1_sistemas.Size = new System.Drawing.Size(56, 13);
             this.label_p1_sistemas.TabIndex = 1;
-            this.label_p1_sistemas.Text = "¿Te gusta resolver problemas con la computadora?";
+            this.label_p1_sistemas.Text = "Pregunta1";
             this.label_p1_sistemas.Click += new System.EventHandler(this.label1_Click);
             // 
             // radio1_si
             // 
             this.radio1_si.AutoSize = true;
             this.radio1_si.Checked = true;
-            this.radio1_si.Location = new System.Drawing.Point(30, 16);
+            this.radio1_si.Location = new System.Drawing.Point(22, 13);
+            this.radio1_si.Margin = new System.Windows.Forms.Padding(2);
             this.radio1_si.Name = "radio1_si";
-            this.radio1_si.Size = new System.Drawing.Size(41, 21);
+            this.radio1_si.Size = new System.Drawing.Size(34, 17);
             this.radio1_si.TabIndex = 4;
             this.radio1_si.TabStop = true;
             this.radio1_si.Text = "Si";
@@ -91,9 +97,10 @@
             // radio1_no
             // 
             this.radio1_no.AutoSize = true;
-            this.radio1_no.Location = new System.Drawing.Point(165, 16);
+            this.radio1_no.Location = new System.Drawing.Point(124, 13);
+            this.radio1_no.Margin = new System.Windows.Forms.Padding(2);
             this.radio1_no.Name = "radio1_no";
-            this.radio1_no.Size = new System.Drawing.Size(47, 21);
+            this.radio1_no.Size = new System.Drawing.Size(39, 17);
             this.radio1_no.TabIndex = 5;
             this.radio1_no.Text = "No";
             this.radio1_no.UseVisualStyleBackColor = true;
@@ -101,9 +108,10 @@
             // radio2_no
             // 
             this.radio2_no.AutoSize = true;
-            this.radio2_no.Location = new System.Drawing.Point(165, 16);
+            this.radio2_no.Location = new System.Drawing.Point(124, 13);
+            this.radio2_no.Margin = new System.Windows.Forms.Padding(2);
             this.radio2_no.Name = "radio2_no";
-            this.radio2_no.Size = new System.Drawing.Size(47, 21);
+            this.radio2_no.Size = new System.Drawing.Size(39, 17);
             this.radio2_no.TabIndex = 8;
             this.radio2_no.Text = "No";
             this.radio2_no.UseVisualStyleBackColor = true;
@@ -112,9 +120,10 @@
             // 
             this.radio2_si.AutoSize = true;
             this.radio2_si.Checked = true;
-            this.radio2_si.Location = new System.Drawing.Point(25, 16);
+            this.radio2_si.Location = new System.Drawing.Point(19, 13);
+            this.radio2_si.Margin = new System.Windows.Forms.Padding(2);
             this.radio2_si.Name = "radio2_si";
-            this.radio2_si.Size = new System.Drawing.Size(41, 21);
+            this.radio2_si.Size = new System.Drawing.Size(34, 17);
             this.radio2_si.TabIndex = 7;
             this.radio2_si.TabStop = true;
             this.radio2_si.Text = "Si";
@@ -123,18 +132,21 @@
             // label_p2_sistemas
             // 
             this.label_p2_sistemas.AutoSize = true;
-            this.label_p2_sistemas.Location = new System.Drawing.Point(126, 167);
+            this.label_p2_sistemas.Location = new System.Drawing.Point(94, 81);
+            this.label_p2_sistemas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_p2_sistemas.Name = "label_p2_sistemas";
-            this.label_p2_sistemas.Size = new System.Drawing.Size(242, 17);
+            this.label_p2_sistemas.Size = new System.Drawing.Size(56, 13);
             this.label_p2_sistemas.TabIndex = 2;
-            this.label_p2_sistemas.Text = "¿Sabes usar bien una computadora?";
+            this.label_p2_sistemas.Text = "Pregunta2";
+            this.label_p2_sistemas.Click += new System.EventHandler(this.label_p2_sistemas_Click);
             // 
             // radio3_no
             // 
             this.radio3_no.AutoSize = true;
-            this.radio3_no.Location = new System.Drawing.Point(165, 16);
+            this.radio3_no.Location = new System.Drawing.Point(124, 13);
+            this.radio3_no.Margin = new System.Windows.Forms.Padding(2);
             this.radio3_no.Name = "radio3_no";
-            this.radio3_no.Size = new System.Drawing.Size(47, 21);
+            this.radio3_no.Size = new System.Drawing.Size(39, 17);
             this.radio3_no.TabIndex = 11;
             this.radio3_no.Text = "No";
             this.radio3_no.UseVisualStyleBackColor = true;
@@ -143,9 +155,10 @@
             // 
             this.radio3_si.AutoSize = true;
             this.radio3_si.Checked = true;
-            this.radio3_si.Location = new System.Drawing.Point(25, 16);
+            this.radio3_si.Location = new System.Drawing.Point(19, 13);
+            this.radio3_si.Margin = new System.Windows.Forms.Padding(2);
             this.radio3_si.Name = "radio3_si";
-            this.radio3_si.Size = new System.Drawing.Size(41, 21);
+            this.radio3_si.Size = new System.Drawing.Size(34, 17);
             this.radio3_si.TabIndex = 10;
             this.radio3_si.TabStop = true;
             this.radio3_si.Text = "Si";
@@ -154,18 +167,20 @@
             // label_p3_sistemas
             // 
             this.label_p3_sistemas.AutoSize = true;
-            this.label_p3_sistemas.Location = new System.Drawing.Point(126, 303);
+            this.label_p3_sistemas.Location = new System.Drawing.Point(94, 156);
+            this.label_p3_sistemas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_p3_sistemas.Name = "label_p3_sistemas";
-            this.label_p3_sistemas.Size = new System.Drawing.Size(226, 17);
+            this.label_p3_sistemas.Size = new System.Drawing.Size(56, 13);
             this.label_p3_sistemas.TabIndex = 3;
-            this.label_p3_sistemas.Text = "¿Te gustaria crear un videojuego?";
+            this.label_p3_sistemas.Text = "Pregunta3";
             // 
             // radio4_no
             // 
             this.radio4_no.AutoSize = true;
-            this.radio4_no.Location = new System.Drawing.Point(165, 16);
+            this.radio4_no.Location = new System.Drawing.Point(124, 13);
+            this.radio4_no.Margin = new System.Windows.Forms.Padding(2);
             this.radio4_no.Name = "radio4_no";
-            this.radio4_no.Size = new System.Drawing.Size(47, 21);
+            this.radio4_no.Size = new System.Drawing.Size(39, 17);
             this.radio4_no.TabIndex = 14;
             this.radio4_no.Text = "No";
             this.radio4_no.UseVisualStyleBackColor = true;
@@ -174,9 +189,10 @@
             // 
             this.radio4_si.AutoSize = true;
             this.radio4_si.Checked = true;
-            this.radio4_si.Location = new System.Drawing.Point(25, 16);
+            this.radio4_si.Location = new System.Drawing.Point(19, 13);
+            this.radio4_si.Margin = new System.Windows.Forms.Padding(2);
             this.radio4_si.Name = "radio4_si";
-            this.radio4_si.Size = new System.Drawing.Size(41, 21);
+            this.radio4_si.Size = new System.Drawing.Size(34, 17);
             this.radio4_si.TabIndex = 13;
             this.radio4_si.TabStop = true;
             this.radio4_si.Text = "Si";
@@ -185,18 +201,20 @@
             // label_p4_sistemas
             // 
             this.label_p4_sistemas.AutoSize = true;
-            this.label_p4_sistemas.Location = new System.Drawing.Point(126, 445);
+            this.label_p4_sistemas.Location = new System.Drawing.Point(94, 232);
+            this.label_p4_sistemas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_p4_sistemas.Name = "label_p4_sistemas";
-            this.label_p4_sistemas.Size = new System.Drawing.Size(352, 17);
+            this.label_p4_sistemas.Size = new System.Drawing.Size(56, 13);
             this.label_p4_sistemas.TabIndex = 4;
-            this.label_p4_sistemas.Text = "¿Consideras que tienes un buen razonamiento lógico?";
+            this.label_p4_sistemas.Text = "Pregunta4";
             // 
             // radio5_no
             // 
             this.radio5_no.AutoSize = true;
-            this.radio5_no.Location = new System.Drawing.Point(165, 16);
+            this.radio5_no.Location = new System.Drawing.Point(124, 13);
+            this.radio5_no.Margin = new System.Windows.Forms.Padding(2);
             this.radio5_no.Name = "radio5_no";
-            this.radio5_no.Size = new System.Drawing.Size(47, 21);
+            this.radio5_no.Size = new System.Drawing.Size(39, 17);
             this.radio5_no.TabIndex = 17;
             this.radio5_no.Text = "No";
             this.radio5_no.UseVisualStyleBackColor = true;
@@ -205,9 +223,10 @@
             // 
             this.radio5_si.AutoSize = true;
             this.radio5_si.Checked = true;
-            this.radio5_si.Location = new System.Drawing.Point(25, 16);
+            this.radio5_si.Location = new System.Drawing.Point(19, 13);
+            this.radio5_si.Margin = new System.Windows.Forms.Padding(2);
             this.radio5_si.Name = "radio5_si";
-            this.radio5_si.Size = new System.Drawing.Size(41, 21);
+            this.radio5_si.Size = new System.Drawing.Size(34, 17);
             this.radio5_si.TabIndex = 16;
             this.radio5_si.TabStop = true;
             this.radio5_si.Text = "Si";
@@ -216,19 +235,22 @@
             // label_p5_sistemas
             // 
             this.label_p5_sistemas.AutoSize = true;
-            this.label_p5_sistemas.Location = new System.Drawing.Point(126, 561);
+            this.label_p5_sistemas.Location = new System.Drawing.Point(94, 302);
+            this.label_p5_sistemas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_p5_sistemas.Name = "label_p5_sistemas";
-            this.label_p5_sistemas.Size = new System.Drawing.Size(308, 17);
+            this.label_p5_sistemas.Size = new System.Drawing.Size(56, 13);
             this.label_p5_sistemas.TabIndex = 5;
-            this.label_p5_sistemas.Text = "¿Te gusta el desarrollo de nuevas tecnologías?";
+            this.label_p5_sistemas.Text = "Pregunta5";
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.radio1_si);
             this.groupBox1.Controls.Add(this.radio1_no);
-            this.groupBox1.Location = new System.Drawing.Point(134, 88);
+            this.groupBox1.Location = new System.Drawing.Point(159, 37);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(218, 43);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(164, 35);
             this.groupBox1.TabIndex = 18;
             this.groupBox1.TabStop = false;
             // 
@@ -236,9 +258,11 @@
             // 
             this.groupBox2.Controls.Add(this.radio2_si);
             this.groupBox2.Controls.Add(this.radio2_no);
-            this.groupBox2.Location = new System.Drawing.Point(134, 215);
+            this.groupBox2.Location = new System.Drawing.Point(159, 119);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(218, 43);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(164, 35);
             this.groupBox2.TabIndex = 19;
             this.groupBox2.TabStop = false;
             // 
@@ -246,9 +270,11 @@
             // 
             this.groupBox3.Controls.Add(this.radio3_si);
             this.groupBox3.Controls.Add(this.radio3_no);
-            this.groupBox3.Location = new System.Drawing.Point(134, 334);
+            this.groupBox3.Location = new System.Drawing.Point(159, 187);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(218, 43);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(164, 35);
             this.groupBox3.TabIndex = 20;
             this.groupBox3.TabStop = false;
             // 
@@ -256,9 +282,11 @@
             // 
             this.groupBox4.Controls.Add(this.radio4_si);
             this.groupBox4.Controls.Add(this.radio4_no);
-            this.groupBox4.Location = new System.Drawing.Point(134, 481);
+            this.groupBox4.Location = new System.Drawing.Point(159, 262);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(218, 43);
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox4.Size = new System.Drawing.Size(164, 35);
             this.groupBox4.TabIndex = 21;
             this.groupBox4.TabStop = false;
             // 
@@ -266,17 +294,39 @@
             // 
             this.groupBox5.Controls.Add(this.radio5_si);
             this.groupBox5.Controls.Add(this.radio5_no);
-            this.groupBox5.Location = new System.Drawing.Point(134, 592);
+            this.groupBox5.Location = new System.Drawing.Point(159, 343);
+            this.groupBox5.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(218, 43);
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox5.Size = new System.Drawing.Size(164, 35);
             this.groupBox5.TabIndex = 22;
             this.groupBox5.TabStop = false;
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(344, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(212, 95);
+            this.listBox1.TabIndex = 23;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(50, 422);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 33);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Atras";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(657, 669);
+            this.ClientSize = new System.Drawing.Size(568, 544);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
@@ -288,6 +338,7 @@
             this.Controls.Add(this.label_p2_sistemas);
             this.Controls.Add(this.label_p1_sistemas);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -329,6 +380,8 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -26,37 +26,125 @@ namespace ProyectoTestEquipo3
             //Cargar archivo prilig
             string[] p = { "-q", "-f", @"test.pl" };
             PlEngine.Initialize(p);
-        }
+            cargarPreguntas("sistemas");
+            
+           
 
+        }
+        int click = 0;
         private void button1_Click(object sender, EventArgs e)
         {
-
+            click++;
             // "cargar" fue declarado en el archivo de prolog
             //PlQuery cargar = new PlQuery("cargar('test.bd')");
             // cargar.NextSolution();
-
 
             /*
                 El ciclo va recorriendo los labels gracias al metodo obtenerElementoPorIndice(), los labels
                 que tienen las preguntas están enumerados del 1 al 5 esto para que puedan recorrerse
             */
 
+            //preguntasSistemas();
+            //preguntasAdministracion();
 
-            //Respondidos guarda los valores de todos los RadioButtons
-            List<String> respondidos = new List<String>();
-            respondidos = responder();
-
-            for (int i = 0; i < 5; i++)
+            if (click < 0) {
+                click = 0;
+            }else
+            if (click > 5)
             {
-                String pregunta = String.Format("consultar('{0}',{1},{2})", obtenerElementoPorIndice(this.Controls,i+1), "sistemas", respondidos[i]);
-                MessageBox.Show(pregunta);
-                PlQuery consulta = new PlQuery(pregunta);
-                MessageBox.Show(consulta.NextSolution().ToString());
+                click = 5;
+            }
+
+            switch (click)
+            {
+                case 0:
+                    cargarPreguntas("sistemas");
+                    
+                    break;
+                case 1:
+                    preguntasSistemas();
+                    cargarPreguntas("administracion");
+                    break;
+                case 2:
+                    cargarPreguntas("gastronomia");
+                    break;
+                case 3:
+                    cargarPreguntas("civil");
+                    break;
+                case 4:
+                    cargarPreguntas("medicina");
+                    break;
+                case 5:
+                    cargarPreguntas("arqueologia");
+                    break;
             }
 
 
 
+
+
+
+        }//button click
+
+        //metodo que consulta en la BD las preguntas de una carrera y las carga en los Lables
+        private void cargarPreguntas(String carrera) {
+            String[] Pa = new string[5];//PREGUNTAS
+            int contador = 0;
+            //listBox1.Items.Clear();
+            PlQuery cargar = new PlQuery("cargar('test.bd')");
+            cargar.NextSolution();
+
+            PlQuery consultas = new PlQuery("pregunta(X,"+carrera+")");
+            
+            foreach (PlQueryVariables z in consultas.SolutionVariables)
+            {
+                  //listBox1.Items.Add(z["X"]);
+                  Pa[contador] = z["X"].ToString();
+               
+                  //MessageBox.Show(Pa[contador]);
+                  contador++;
+            }
+
+            label_p1_sistemas.Text = Pa[0];
+            label_p2_sistemas.Text = Pa[1];
+            label_p3_sistemas.Text = Pa[2];
+            label_p4_sistemas.Text = Pa[3];
+            label_p5_sistemas.Text = Pa[4];
+            contador = 0;
+
         }
+        private void preguntasSistemas() {
+            List<String> respondidos = new List<String>();
+            respondidos = responder();
+            //Respondidos guarda los valores de todos los RadioButtons
+
+            for (int i = 0; i < 5; i++)
+            {
+                String pregunta = String.Format("consultar('{0}',{1},{2})", obtenerElementoPorIndice(this.Controls, i + 1), "sistemas", respondidos[i]);
+               
+                MessageBox.Show(pregunta);
+                PlQuery consulta = new PlQuery(pregunta);
+                 MessageBox.Show(consulta.NextSolution().ToString());
+            }
+        }
+
+
+        private void preguntasAdministracion()
+        {
+        }
+        private void preguntasGastronomia() 
+        {
+        }
+        private void preguntasCivil()
+        {
+        }
+        private void preguntasMeduicina()
+        {
+        }
+        private void preguntasArqueologia()
+        {
+        }
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -86,6 +174,51 @@ namespace ProyectoTestEquipo3
             return controls.OfType<Control>()
                            .Where(c => c.TabIndex == index)
                            .Select(c => c.Text).First();
+        }
+
+        private void label_p2_sistemas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            click--;
+
+
+            if (click < 0)
+            {
+                click = 0;
+                
+            }else
+
+            if (click > 5)
+            {
+                click = 5;
+            }
+
+            switch (click)
+            {
+                case 0:
+                    cargarPreguntas("sistemas");
+                    break;
+                case 1:
+                    cargarPreguntas("administracion");
+                    break;
+                case 2:
+                    cargarPreguntas("gastronomia");
+                    break;
+                case 3:
+                    cargarPreguntas("civil");
+                    break;
+                case 4:
+                    cargarPreguntas("medicina");
+                    break;
+                case 5:
+                    cargarPreguntas("arqueologia");
+                    break;
+            }
+
         }
     }
 }
